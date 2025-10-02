3e1ba0ebe70e7c9035623e37da6140d3a04f9528
armrcode <98815717+ARmrCode@users.noreply.github.com>
2025-10-02 15:39:23 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisService } from './redis/redis.service';
+import { MetricsController } from './metrics/metrics.controller';
 
 @Module({
 	imports: [
